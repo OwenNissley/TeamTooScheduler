a6fbab0b314b9859c4d16f454a8275dad37b29ad
CSB715 <126495585+CSB715@users.noreply.github.com>
2025-03-14 16:19:28 -0400

    Setting up file read and write

--- src/main/java/edu/gcc/comp350/teamtoo/FilterType.java
+++
@@ -1,8 +0,0 @@
-package edu.gcc.comp350.teamtoo;
-
-public enum FilterType {
-    TIME,         // Filter based on time
-    DAY_OF_WEEK,  // Filter based on days of the week
-    SEMESTER,     // Filter based on semester
-    COURSE_CODE;  // Filter based on course codes
-}
